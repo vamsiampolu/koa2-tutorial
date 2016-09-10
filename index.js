c0c7050bb73e92d6c27442f07180d632e3aec614
vamsiampolu <vamsideepak03@gmail.com>
2016-09-11 03:44:21 +1000

    Adds multiple middleware, figures out the flow of control through middleware

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ import Koa from 'koa'
 
 const app = new Koa()
 
+const firstMiddleware = async (ctx,next) => {
+  console.log('Going Down')
+  await next()
+  console.log('Going More Up')
+}
+
+const secondMiddleware = async (ctx,next) => {
+  console.log('Going More Down')
+  await next()
+  console.log('Going Up')
+}
+
 const responseMiddleware = async (ctx) => {
   ctx.body = 'Hello World!'
 }
 
+app.use(firstMiddleware)
+app.use(secondMiddleware)
 app.use(responseMiddleware)
 app.listen(3000)
