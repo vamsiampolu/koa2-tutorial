460e4dfb2870e064bfbf03c9b9bd11ef333b86f4
vamsiampolu <vamsideepak03@gmail.com>
2016-09-11 04:16:44 +1000

    Adds logger, response time and content-length middleware

--- index.js
+++ index.js
@@ -3,6 +3,47 @@ import Koa from 'koa'
 
 const app = new Koa()
 
+
+const contentLength = () => {
+  return async function(ctx,next) {
+    await next()
+    if(!ctx.body) {
+        return
+    }
+    ctx.set('Content-Length',ctx.body.length)
+  }
+}
+
+const body = () => {
+  return async function(ctx,next) {
+    const {path} = ctx
+    if(path !== '/') {
+      return
+    }
+    ctx.body = 'Hello World!'
+  }
+}
+
+const logger = (format) => {
+  format = format || "':method' ':url'"
+  return async function(ctx,next) {
+    const str = format
+                  .replace(':method',ctx.method)
+                  .replace(':url',ctx.url)
+    console.log(str)
+    await next()
+  }
+}
+
+const responseTime = () => {
+  return async function(ctx,next) {
+    var start = new Date()
+    await next()
+    var ms = new Date() - start
+    ctx.set('X-Response-Time',ms+'ms')
+  }
+}
+
 const firstMiddleware = async (ctx,next) => {
   console.log('Going Down')
   await next()
@@ -19,7 +60,10 @@ const responseMiddleware = async (ctx) => {
   ctx.body = 'Hello World!'
 }
 
+app.use(responseTime())
 app.use(firstMiddleware)
 app.use(secondMiddleware)
+app.use(logger())
 app.use(responseMiddleware)
+
 app.listen(3000)
